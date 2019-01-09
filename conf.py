46940f555fe434b65913c2a8a94d4349b27574de
Jens Willemsens <jens@jensw.be>
2019-01-09 21:23:20 +0100

    Re-enable image scaling to 4 + fix CSS typo "socil"

--- conf.py
+++ conf.py
@@ -1266,7 +1266,7 @@ USE_CDN = False
 USE_BUNDLES = True
 
 # Plugins you don't want to use. Be careful :-)
-DISABLED_PLUGINS = ["render_galleries", "render_taxonomies", "scale_images"]
+DISABLED_PLUGINS = ["render_galleries", "render_taxonomies"]
 
 # Special settings to disable only parts of the indexes plugin.
 # Use with care.
