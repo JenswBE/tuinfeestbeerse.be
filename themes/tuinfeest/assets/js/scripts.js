ab0c92ed4fec4ac02ded8cacad2fff475d9b3adb
Jens Willemsens <jens@jensw.be>
2019-01-10 22:01:01 +0100

    Cleanup + smooth links + content section

--- themes/tuinfeest/assets/js/scripts.js
+++ themes/tuinfeest/assets/js/scripts.js
@@ -1,12 +1,7 @@
 (function($) {
     "use strict";
 
-    /*----------------------------
-     wow js active
-    ------------------------------ */
-    new WOW().init();
-  
-  // responsive-menu tigger
+  // Responsive-menu trigger
     $(".menu").on('click', function() {
         $(".responsive-menu-area").toggleClass("active");
     });
@@ -14,7 +9,7 @@
     $('ul.metismenu').metisMenu({
     });
 
-    // slider-active
+    // Slider-active
     $('.slider-active').owlCarousel({
         margin: 0,
         loop: true,
@@ -40,32 +35,7 @@
         }
     });
 
-    // slider-active
-    $('.slider-active2').owlCarousel({
-        margin: 0,
-        loop: true,
-        nav: true,
-        smartSpeed: 1200,
-        navText: ['<i class="fa fa-angle-down"></i>', '<i class="fa fa-angle-up"></i>'],
-        URLhashListener: true,
-        startPosition: 'URLHash',
-        responsive: {
-            0: {
-                items: 1,
-            },
-            450: {
-                items: 1,
-            },
-            768: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
-    });
-
-    // slider-active
+    // Slider-active
     $(".slider-active").on('translate.owl.carousel', function() {
         $('.slider-items h2').removeClass('fadeInLeft animated').hide();
         $('.slider-items p').removeClass('fadeInUp animated').hide();
@@ -95,35 +65,7 @@
     }
     sliderBgSetting()
 
-    // test-active
-    $('.test-active').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 4000,
-        nav: false,
-        smartSpeed: 800,
-        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-        responsive: {
-            0: {
-                items: 1
-            },
-            450: {
-                items: 1
-            },
-            768: {
-                items: 1
-            },
-            1000: {
-                items: 2
-            }
-        }
-    });
-
-
-
-
-    // // stickey menu
+    // Sticky menu
     $(window).on('scroll', function() {
         var scroll = $(window).scrollTop(),
             mainHeader = $('#sticky-header'),
@@ -147,64 +89,6 @@
         animation: 'fade'
     });
 
-    /*--
-    Magnific Popup
-    ------------------------*/
-    $('.popup').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true
-        }
-
-    });
-
-    $('.video-popup').magnificPopup({
-        type: 'iframe',
-        gallery: {
-            enabled: true
-        }
-
-    });
-
-    // counter up
-    $('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
-
-
-    $('.grid').imagesLoaded(function() {
-        // init Isotope
-        var $grid = $('.grid').isotope({
-            itemSelector: '.project',
-            percentPosition: true,
-            masonry: {
-                // use outer width of grid-sizer for columnWidth
-                columnWidth: '.project',
-            }
-        });
-    });
-
-    /*-------------------------------------------------------
-        blog details
-    -----------------------------------------------------*/
-    if ($(".choos-area").length) {
-        var post = $(".choost-img");
-
-        post.each(function() {
-            var $this = $(this);
-            var entryMedia = $this.find("img");
-            var entryMediaPic = entryMedia.attr("src");
-
-            $this.css({
-                backgroundImage: "url(" + entryMediaPic + ")",
-                backgroundSize: "cover",
-                backgroundPosition: "center center",
-            })
-        })
-    }
-
     function setTwoColEqHeight($col1, $col2) {
         var firstCol = $col1,
             secondCol = $col2,
@@ -222,7 +106,7 @@
         }
     }
 
-    // smooth-scrolling
+    // Smooth-scrolling
     function smoothScrolling($links, $topGap) {
         var links = $links;
         var topGap = $topGap;
@@ -243,14 +127,12 @@
     }
 
     $(window).on("load", function() {
-        setTwoColEqHeight($(".choost-img"), $(".choos-content"));
-        smoothScrolling($(".scrolldown a[href^='#']"), -2);
-
+        smoothScrolling($(".smooth-links a[href^='#']"), -2);
     });
 
 
     /*---------------------
-     countdown
+     Countdown
     --------------------- */
     $('[data-countdown]').each(function() {
         var $this = $(this),
@@ -258,75 +140,6 @@
         $this.countdown(finalDate, function(event) {
             $this.html(event.strftime('<span class="cdown days"><span class="time-count">%-D</span> <p>Dagen</p></span> <span class="cdown hour"><span class="time-count">%-H</span> <p>Uren</p></span> <span class="cdown minutes"><span class="time-count">%M</span> <p>Minuten</p></span> <span class="cdown second"> <span><span class="time-count">%S</span> <p>Seconden</p></span>'));
         });
-    });
-
-    /*==================================
-            LOAD MORE JQUERY
-    ================================== */
-    var list1 = $(".moreload");
-    var numToShow1 = 2;
-    var button1 = $(".loadmore-btn");
-    var numInList1 = list1.length;
-
-    list1.hide();
-    if (numInList1 > numToShow1) {
-        button1.show();
-    }
-    list1.slice(0, numToShow1).show();
-    button1.on('click', function() {
-        var showing1 = list1.filter(':visible').length;
-        list1.slice(showing1 - 1, showing1 + numToShow1).fadeIn();
-        var nowShowing1 = list1.filter(':visible').length;
-        if (nowShowing1 >= numInList1) {
-            button1.hide();
-        }
-    });
-
-
-    /*---------------------
-    // Ajax Contact Form
-    --------------------- */
-
-    $('.cf-msg').hide();
-    $('form#cf button#submit').on('click', function() {
-        var fname = $('#fname').val();
-        var subject = $('#subject').val();
-        var email = $('#email').val();
-        var msg = $('#msg').val();
-        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
-        if (!regex.test(email)) {
-            alert('Please enter valid email');
-            return false;
-        }
-
-        fname = $.trim(fname);
-        subject = $.trim(subject);
-        email = $.trim(email);
-        msg = $.trim(msg);
-
-        if (fname != '' && email != '' && msg != '') {
-            var values = "fname=" + fname + "&subject=" + subject + "&email=" + email + " &msg=" + msg;
-            $.ajax({
-                type: "POST",
-                url: "mail.php",
-                data: values,
-                success: function() {
-                    $('#fname').val('');
-                    $('#subject').val('');
-                    $('#email').val('');
-                    $('#msg').val('');
-
-                    $('.cf-msg').fadeIn().html('<div class="alert alert-success"><strong>Success!</strong> Email has been sent successfully.</div>');
-                    setTimeout(function() {
-                        $('.cf-msg').fadeOut('slow');
-                    }, 4000);
-                }
-            });
-        } else {
-            $('.cf-msg').fadeIn().html('<div class="alert alert-danger"><strong>Warning!</strong> Please fillup the informations correctly.</div>')
-        }
-        return false;
     });
 
 })(jQuery);
