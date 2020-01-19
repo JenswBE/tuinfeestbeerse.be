fed83b13404291ac947c0edd516fd215bab635f3
Jens Willemsens <jens@jensw.be>
2020-01-19 13:02:32 +0100

    Add Melthon watcher for easier development

---
+++ melthon_watch.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# Helper to rerender site when files changed.
+# Execute with ./melthon_watch.sh [build options]
+
+# Check if inotifywait is available
+# https://stackoverflow.com/questions/592620/how-can-i-check-if-a-program-exists-from-a-bash-script
+command -v inotifywait >/dev/null 2>&1 || { echo >&2 "inotifywait is missing, please install with 'sudo apt install inotify-tools'"; exit 1; }
+
+# Start watching
+echo "'melthon build ${*}' will be executed on file changes."
+echo "Start watching ..."
+inotifywait -qmr --exclude "output/.*" --event close_write --format "build ${*}" . | xargs -L 1 melthon
